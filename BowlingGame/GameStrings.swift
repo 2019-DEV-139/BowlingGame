dcee0917e3127fd02646e4ddfabdb6398e28147b
2019_DEV_139 <2019_DEV_139@zeljkos-macbook-pro-2.local>
2019-03-16 13:44:37 +0100

    Defined test games

---
+++ BowlingGame/GameStrings.swift
@@ -0,0 +1,15 @@
+//
+//  GameStrings.swift
+//  BowlingGame
+//
+//  Created by 2019_DEV_139 on 16/03/2019.
+//  Copyright © 2019 2019_DEV_139. All rights reserved.
+//
+
+import Foundation
+
+struct GameStrings {
+    static var perfect = "X X X X X X X X X X X X"
+    static var nines = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"
+    static var saves = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5"
+}
