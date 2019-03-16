bb4a437fd7dbc837c06ac5fd03661ebc90a65d88
2019_DEV_139 <2019_DEV_139@zeljkos-macbook-pro-2.local>
2019-03-16 13:46:41 +0100

    Added abstract `SimpleCalculator` class

---
+++ BowlingGame/SimpleCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleCalculator.swift
+//  BowlingGame
+//
+//  Created by 2019_DEV_139 on 16/03/2019.
+//  Copyright © 2019 2019_DEV_139. All rights reserved.
+//
+
+import Foundation
+
+struct SimpleCalculator {
+    static func calculateScore(from game: String) -> Int {
+        return 0
+    }
+}
